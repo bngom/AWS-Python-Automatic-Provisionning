1b83a1531e9ad55cfbb8242c6d5cb038b4d6f5de
Barthélemy Ngom <33115128+bngom@users.noreply.github.com>
2020-07-17 17:04:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -71,8 +71,8 @@ def create_s3Bucket():
 if __name__ == "__main__":
     config_path = os.path.join(os.getcwd(), "configs")
     parser = argparse.ArgumentParser(description="AWS EC2 and S3")
-    parser.add_argument("-c", "--config", default=os.path.join(config_path, "jupiter-configs.json"), metavar="Config", type=str)
-    parser.add_argument("-u", "--userdata", default=os.path.join(config_path, "jupiter-user-data"), metavar="User Data", type=str)
+    parser.add_argument("-c", "--config", default=os.path.join(config_path, "configs.json"), metavar="Config", type=str)
+    parser.add_argument("-u", "--userdata", default=os.path.join(config_path, "user-data"), metavar="User Data", type=str)
     args = parser.parse_args()
 
     run_instance(args.config, args.userdata)
