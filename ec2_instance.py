b79544d6f55f6693b8fed8f67620a2b4ddb12aeb
Barthelemy NGOM <barthe.ngom@gmail.com>
2020-07-25 10:53:50 +0200

    update terminate instance

--- ec2_instance.py
+++ ec2_instance.py
@@ -7,7 +7,8 @@ class Instance:
     """
     Class to handle EC2 instance automatic provisioning.
     """
-    def __init__(self, config_file_path, user_data_file_path):
+
+    def __init__(self, config_file_path, user_data_file_path=None):
         """
         :param config_file_path: path to a JSON config file containing the EC2 instance parameters
         :param user_data_file_path: path to user data script
@@ -15,9 +16,19 @@ class Instance:
         self._config_file_path = config_file_path
         self._user_data_file_path = user_data_file_path
 
+    def get_config(self):
+        with open(self._config_file_path, 'r') as f:
+            configs = json.load(f)
+        return configs
+
+    def get_user_data(self):
+        with open(self._user_data_file_path, 'r') as f:
+            user_data = f.read()
+        return user_data
+
     def launch_instance(self, EC2, config, user_data):
         """
-        :param EC2: An instance of the class
+        :param EC2:
         :param config: configuration data for the EC2 instance
         :param user_data: user data script to execute at launch
         :return: a dictionary with the id of the instance created...
@@ -44,24 +55,68 @@ class Instance:
             print('Error:', e)
 
         return {
-                    "InstanceID": instance_id,
-                    "EC2Response": new_instance_resp
+            "InstanceID": instance_id,
+            "EC2Response": new_instance_resp
         }
 
     def create(self):
         """
         Set the context and Trigger an instance creation.
         """
-        input("You are going to deploy an Jupiter Notebook on a EC2 instance. Press any key to continue...")
-        # get the configuration for the EC2 instance
+        input("You are going to deploy an EC2 instance. Press any key to continue...")
 
-        with open(self._config_file_path, 'r') as f:
-            configs = json.load(f)
+        # get the configuration for the EC2 instance
+        configs = self.get_config()
 
         # get user data
-        with open(self._user_data_file_path, 'r') as f:
-            user_data = f.read()
+        user_data = self.get_user_data()
 
         EC2 = boto3.client('ec2', region_name=configs['region'])
         result = self.launch_instance(EC2, configs, user_data)
         print(f"[INFO] Instance launched instance-id: '{result['InstanceID']}'")
+
+    def terminate_instance(self):
+        instance_list = []
+        configs = self.get_config()
+        EC2 = boto3.client('ec2', region_name=configs['region'])
+        response = EC2.describe_instances()
+        # Check existence of at least one instance
+        instance_state_code = {"pending": 0, "running": 16, "shutting - down": 32, "terminated": 48, "stopping": 64, "stopped": 80}
+        if len(response["Reservations"]) !=0:
+            for reservation in response["Reservations"]:
+                for instance in reservation["Instances"]:
+                    # [TO DO] use instance['Tags'] to delete specific instances
+                    # if instance is in running or stopped state
+                    if instance['State']['Code'] in (instance_state_code["running"], instance_state_code["stopped"]):
+                        instance_list.append(instance['InstanceId'])
+                        print(f"[INFO] Found '{instance['State']['Name']}' instance '{instance['InstanceId']}'")
+            # print(instance_list)
+            if len(instance_list) != 0:
+                instance_to_delete = input(f"[DELETE INSTANCE] Type in instance id to delete: ")
+                if instance_to_delete in instance_list:
+                    delete = input(f"[DELETE INSTANCE] '{instance_to_delete}' is running: terminating it[Y/n]")
+                    if delete == "Y":
+                        try:
+                            ec2_resp = EC2.terminate_instances(
+                                InstanceIds=[
+                                    instance_to_delete,
+                                ]
+                            )
+                            # [TO DO] if current state is terminated, exit(1)
+                            return {"Instance ID": ec2_resp['TerminatingInstances'][0]['InstanceId'],
+                                    "CurrentState": ec2_resp['TerminatingInstances'][0]['CurrentState']
+                                    }
+                        except ClientError as e:
+                            print("Error: ", e)
+                    else:
+                        print("[INFO] No instance has been removed")
+                        exit(0)
+                else:
+                    print("[INFO] Wrong instance id!!!")
+                    exit(1)
+            else:
+                print("[INFO] To be removed instance should be in running or stopped state")
+                exit(0)
+        else:
+            print("[INFO] No Reservation - No instance to remove")
+            exit(0)
--- main.py
+++ main.py
@@ -4,64 +4,15 @@ import json
 import os
 
 
-def get_config(user_data_path):
-    with open(user_data_path, 'r') as f:
-        configs = json.load(f)
-    return configs
-
-
 def run_instance(config, user_data):
     instance = Instance(config, user_data)
     instance_creation_response = instance.create()
-    print(instance_creation_response)
 
-# put in the class
-def terminate_instance(config_path):
-    instance_list = []
-    configs = get_config(config_path)
-    EC2 = boto3.client('ec2', region_name=configs['region'])
-    response = EC2.describe_instances()
-    #print(response["Reservations"])
-    # Check existence of at least one instance
-    if response["Reservations"] is not []:
-        for reservation in response["Reservations"]:
-            for instance in reservation["Instances"]:
-                # [TO DO] use instance['Tags'] to delete specific instances
-                #if instance is in running [or stopped state to add later]
-                if instance['State']['Code'] == 16:
-                    instance_list.append(instance['InstanceId'])
-                    print(f"[INFO] Found '{instance['State']['Name']}' instance '{instance['InstanceId']}'")
-        #print(instance_list)
-        if instance_list is not []:
-            instance_to_delete = input(f"[DELETE INSTANCE] Type in instance id to delete: ")
-            if instance_to_delete in instance_list:
-                delete = input(f"[DELETE INSTANCE] '{instance_to_delete}' is running: terminating it[Y/n]")
-                if delete == "Y":
-                    try:
-                        ec2_resp = EC2.terminate_instances(
-                            InstanceIds=[
-                                instance_to_delete,
-                            ]
-                        )
-                        #[TO DO] if current state is terminated, exit(1)
-                        return {"Instance ID": ec2_resp['TerminatingInstances'][0]['InstanceId'],
-                                "CurrentState": ec2_resp['TerminatingInstances'][0]['CurrentState']
-                        }
-                    except ClientError as e:
-                        print("Error: ", e)
-                else:
-                    print("[INFO] No instance has been removed")
-                    exit(0)
-            else:
-                print("[INFO] Wrong instance number!!!")
-                exit(1)
-        else:
-            print("[**INFO] No instance to remove")
-            exit(0)
-    else:
-        print("[INFO] No instance to remove")
-        exit(0)
 
+def terminate_instance(config):
+    instance = Instance(config)
+    response = instance.terminate_instance()
+    return response
 
 def create_s3Bucket():
     """[TO DO]"""
@@ -75,5 +26,5 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--userdata", default=os.path.join(config_path, "user-data"), metavar="User Data", type=str)
     args = parser.parse_args()
 
-    run_instance(args.config, args.userdata)
-    #terminate_instance(args.config)
+    #run_instance(args.config, args.userdata)
+    terminate_instance(args.config)
